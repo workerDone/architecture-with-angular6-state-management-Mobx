dde8ba89daf18cc8a9b69805115bc7b5114b7c27
Mykola <chmutkola@gmail.com>
2018-06-23 20:36:35 +0300

    Generics ts

--- src/app/pages/home/components/market/market.component.ts
+++ src/app/pages/home/components/market/market.component.ts
@@ -5,6 +5,10 @@ import { HomeService } from '../../services/home/home.service';
 
 import { TeamAggregatedModel } from '../../../../modules/teams/models/team-aggregated/team-aggregated.model';
 
+class GenericNumber<T> {
+  zeroValue: T;
+  add: (x: T, y: T) => T;
+}
 @Component({
   selector: 'app-market',
   templateUrl: './market.component.html',
@@ -12,11 +16,23 @@ import { TeamAggregatedModel } from '../../../../modules/teams/models/team-aggre
 })
 export class MarketComponent implements OnInit {
 
+  text: string;
+  num: number;
+  myIdentity;
   constructor(
     private homeService: HomeService
   ) { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.text = this.identity<string>('hello');
+    this.num = this.identity<number>(42);
+    this.myIdentity = new GenericNumber<number>();
+    this.myIdentity.zeroValue = 9;
+    this.myIdentity.add = (x, y) => x + y;
+
+    console.log(this.myIdentity.add(this.myIdentity.zeroValue, 6));
+    console.log(typeof this.text, typeof this.num);
+   }
 
   @action buy(team: TeamAggregatedModel) {
     this.homeService.buy(team);
@@ -25,4 +41,9 @@ export class MarketComponent implements OnInit {
   @computed get teamsAggregated(): TeamAggregatedModel[] {
     return this.homeService.teamsAggregated;
   }
+
+  identity<T>(arg: T): T {
+    return arg;
+  }
+
 }
